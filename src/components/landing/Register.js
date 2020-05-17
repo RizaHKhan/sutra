3beabe419eb1cd1dbf316b4a164806fe35418c7b
RizaHKhan <khanriza@gmail.com>
2020-05-17 12:33:49 -0400

    created register component

--- src/components/landing/Register.js
+++ src/components/landing/Register.js
@@ -1,140 +1,143 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Register = () => {
-  return (
-    <div className="form-box login-register-form-element">
-      {/* FORM BOX DECORATION */}
-      <img
-        className="form-box-decoration"
-        src=''
-        alt="rocket"
-      />
-      {/* /FORM BOX DECORATION */}
-      {/* FORM BOX TITLE */}
-      <h2 className="form-box-title">Create your Account!</h2>
-      {/* /FORM BOX TITLE */}
-      {/* FORM */}
-      <form className="form">
-        {/* FORM ROW */}
-        <div className="form-row">
-          {/* FORM ITEM */}
-          <div className="form-item">
-            {/* FORM INPUT */}
-            <div className="form-input">
-              <label htmlFor="register-email">Your Email</label>
-              <input type="text" id="register-email" name="register_email" />
-            </div>
-            {/* /FORM INPUT */}
-          </div>
-          {/* /FORM ITEM */}
-        </div>
-        {/* /FORM ROW */}
-        {/* FORM ROW */}
-        <div className="form-row">
-          {/* FORM ITEM */}
-          <div className="form-item">
-            {/* FORM INPUT */}
-            <div className="form-input">
-              <label htmlFor="register-username">Username</label>
-              <input
-                type="text"
-                id="register-username"
-                name="register_username"
-              />
-            </div>
-            {/* /FORM INPUT */}
-          </div>
-          {/* /FORM ITEM */}
-        </div>
-        {/* /FORM ROW */}
-        {/* FORM ROW */}
-        <div className="form-row">
-          {/* FORM ITEM */}
-          <div className="form-item">
-            {/* FORM INPUT */}
-            <div className="form-input">
-              <label htmlFor="register-password">Password</label>
-              <input
-                type="password"
-                id="register-password"
-                name="register_password"
-              />
-            </div>
-            {/* /FORM INPUT */}
-          </div>
-          {/* /FORM ITEM */}
-        </div>
-        {/* /FORM ROW */}
-        {/* FORM ROW */}
-        <div className="form-row">
-          {/* FORM ITEM */}
-          <div className="form-item">
-            {/* FORM INPUT */}
-            <div className="form-input">
-              <label htmlFor="register-password-repeat">Repeat Password</label>
-              <input
-                type="password"
-                id="register-password-repeat"
-                name="register_password_repeat"
-              />
-            </div>
-            {/* /FORM INPUT */}
-          </div>
-          {/* /FORM ITEM */}
-        </div>
-        {/* /FORM ROW */}
-        {/* FORM ROW */}
-        <div className="form-row">
-          {/* FORM ITEM */}
-          <div className="form-item">
-            {/* CHECKBOX WRAP */}
-            <div className="checkbox-wrap">
-              <input
-                type="checkbox"
-                id="register-newsletter"
-                name="register_newsletter"
-                defaultChecked
-              />
-              {/* CHECKBOX BOX */}
-              <div className="checkbox-box">
-                {/* ICON CROSS */}
-                <svg className="icon-cross">
-                  <use xlinkHref="#svg-cross" />
-                </svg>
-                {/* /ICON CROSS */}
+class Register extends Component {
+  render() {
+    return (
+      <div className="form-box login-register-form-element">
+        {/* FORM BOX DECORATION */}
+        <img
+          className="form-box-decoration"
+          src="img/landing/rocket.png"
+          alt="rocket"
+        />
+        {/* /FORM BOX DECORATION */}
+        {/* FORM BOX TITLE */}
+        <h2 className="form-box-title">Create your Account!</h2>
+        {/* /FORM BOX TITLE */}
+        {/* FORM */}
+        <form className="form">
+          {/* FORM ROW */}
+          <div className="form-row">
+            {/* FORM ITEM */}
+            <div className="form-item">
+              {/* FORM INPUT */}
+              <div className="form-input">
+                <label htmlFor="register-email">Your Email</label>
+                <input type="text" id="register-email" name="register_email" />
               </div>
-              {/* /CHECKBOX BOX */}
-              <label htmlFor="register-newsletter">
-                Send me news and updates via email
-              </label>
+              {/* /FORM INPUT */}
             </div>
-            {/* /CHECKBOX WRAP */}
+            {/* /FORM ITEM */}
           </div>
-          {/* /FORM ITEM */}
-        </div>
-        {/* /FORM ROW */}
-        {/* FORM ROW */}
-        <div className="form-row">
-          {/* FORM ITEM */}
-          <div className="form-item">
-            {/* BUTTON */}
-            <button className="button medium primary">Register Now!</button>
-            {/* /BUTTON */}
+          {/* /FORM ROW */}
+          {/* FORM ROW */}
+          <div className="form-row">
+            {/* FORM ITEM */}
+            <div className="form-item">
+              {/* FORM INPUT */}
+              <div className="form-input">
+                <label htmlFor="register-username">Username</label>
+                <input
+                  type="text"
+                  id="register-username"
+                  name="register_username"
+                />
+              </div>
+              {/* /FORM INPUT */}
+            </div>
+            {/* /FORM ITEM */}
           </div>
-          {/* /FORM ITEM */}
-        </div>
-        {/* /FORM ROW */}
-      </form>
-      {/* /FORM */}
-      {/* FORM TEXT */}
-      <p className="form-text">
-        You'll receive a confirmation email in your inbox with a link to
-        activate your account. If you have any problems,{" "}
-        <a href="#">contact us</a>!
-      </p>
-      {/* /FORM TEXT */}
-    </div>
-  );
-};
-
+          {/* /FORM ROW */}
+          {/* FORM ROW */}
+          <div className="form-row">
+            {/* FORM ITEM */}
+            <div className="form-item">
+              {/* FORM INPUT */}
+              <div className="form-input">
+                <label htmlFor="register-password">Password</label>
+                <input
+                  type="password"
+                  id="register-password"
+                  name="register_password"
+                />
+              </div>
+              {/* /FORM INPUT */}
+            </div>
+            {/* /FORM ITEM */}
+          </div>
+          {/* /FORM ROW */}
+          {/* FORM ROW */}
+          <div className="form-row">
+            {/* FORM ITEM */}
+            <div className="form-item">
+              {/* FORM INPUT */}
+              <div className="form-input">
+                <label htmlFor="register-password-repeat">
+                  Repeat Password
+                </label>
+                <input
+                  type="password"
+                  id="register-password-repeat"
+                  name="register_password_repeat"
+                />
+              </div>
+              {/* /FORM INPUT */}
+            </div>
+            {/* /FORM ITEM */}
+          </div>
+          {/* /FORM ROW */}
+          {/* FORM ROW */}
+          <div className="form-row">
+            {/* FORM ITEM */}
+            <div className="form-item">
+              {/* CHECKBOX WRAP */}
+              <div className="checkbox-wrap">
+                <input
+                  type="checkbox"
+                  id="register-newsletter"
+                  name="register_newsletter"
+                  defaultChecked
+                />
+                {/* CHECKBOX BOX */}
+                <div className="checkbox-box">
+                  {/* ICON CROSS */}
+                  <svg className="icon-cross">
+                    <use xlinkHref="#svg-cross" />
+                  </svg>
+                  {/* /ICON CROSS */}
+                </div>
+                {/* /CHECKBOX BOX */}
+                <label htmlFor="register-newsletter">
+                  Send me news and updates via email
+                </label>
+              </div>
+              {/* /CHECKBOX WRAP */}
+            </div>
+            {/* /FORM ITEM */}
+          </div>
+          {/* /FORM ROW */}
+          {/* FORM ROW */}
+          <div className="form-row">
+            {/* FORM ITEM */}
+            <div className="form-item">
+              {/* BUTTON */}
+              <button className="button medium primary">Register Now!</button>
+              {/* /BUTTON */}
+            </div>
+            {/* /FORM ITEM */}
+          </div>
+          {/* /FORM ROW */}
+        </form>
+        {/* /FORM */}
+        {/* FORM TEXT */}
+        <p className="form-text">
+          You'll receive a confirmation email in your inbox with a link to
+          activate your account. If you have any problems,{" "}
+          <a href="#">contact us</a>!
+        </p>
+        {/* /FORM TEXT */}
+      </div>
+    );
+  }
+}
 export default Register;
